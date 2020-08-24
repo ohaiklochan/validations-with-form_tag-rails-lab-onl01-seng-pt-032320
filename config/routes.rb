efb25c85920198f70035ee740703102064553354
Chloe Steffens <caforonda103@yahoo.com>
2020-08-24 15:07:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:show, :new, :edit, :create, :update]
+  resources :authors, only: [:show, :new, :edit, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
