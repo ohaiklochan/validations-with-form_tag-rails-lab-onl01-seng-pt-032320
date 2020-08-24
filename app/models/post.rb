9745d759297520bc500185ebd5afce33f0c05950
Chloe Steffens <caforonda103@yahoo.com>
2020-08-24 15:07:49 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
